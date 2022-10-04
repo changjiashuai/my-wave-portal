143a5caf3a45f913b28de0ec99297f7ca8cb5026
changjiashuai <changjiashuai@gmail.com>
2022-10-04 14:47:16 +0800

    deploy to testnet by QuickNode.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts){
-    console.log(account.address);
-  }
+    for (const account of accounts) {
+        console.log(account.address);
+    }
 });
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        goerli: {
+            url: process.env.DEV_QUICKNODE_KEY,
+            accounts: [process.env.PRIVATE_KEY]
+        },
+        mainnet: {
+            url: process.env.PROD_QUICKNODE_KEY,
+            accounts: [process.env.PRIVATE_KEY]
+        }
+    }
 };
